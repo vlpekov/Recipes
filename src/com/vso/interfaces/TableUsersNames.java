dcf3247abe3da8970a6fabf45e8a2cd7c228260e
vlpekov <vladdey@gmail.com>
2017-06-15 14:58:11 +0300

    new interface: TableUsersNames

--- src/com/vso/interfaces/TableUsersNames.java
+++ src/com/vso/interfaces/TableUsersNames.java
@@ -1,13 +1,22 @@
 package com.vso.interfaces;
 
-public interface TableUsersNames{
+public interface TableUsersNames extends DatabaseNames {
 	String tableUsersName = "users";
-	String tableUsers = "";
-	String tableUsers = "";
-	String tableUsers = "";
-	String tableUsers = "";
-	String tableUsers = "";
-	String tableUsers = "";
-	String tableUsers = "";
-	
+	String tableUsersId = "id";
+	String tableUsersUsername = "username";
+	String tableUsersPassword = "password";
+	String tableUsersFirstName = "first_name";
+	String tableUsersLastName = "last_name";
+	String tableUsersEmail = "email";
+	String tableUsersRegistrationDate = "date_of_registration";
+	String tableUsersGender = "gender";
+	String tableUsersProfilPictureFile = "image";
+	String tableUsersProfilPictureName = "image_name";
+
+	String crateTableComand = "CREATE TABLE `" + databaseName + "`.`" + tableUsersName + "` (" + tableUsersId
+			+ " SERIAL PRIMARY KEY NOT NULL, " + tableUsersUsername + " TEXT NOT NULL, " + tableUsersPassword
+			+ " TEXT NOT NULL, " + tableUsersFirstName + " TEXT NOT NULL, " + tableUsersLastName + " TEXT NOT NULL, "
+			+ tableUsersEmail + " TEXT NOT NULL, " + tableUsersRegistrationDate + " DATE NOT NULL, " + tableUsersGender
+			+ " TEXT NOT NULL," + tableUsersProfilPictureFile + " MEDIUMBLOB NOT NULL, " + tableUsersProfilPictureName
+			+ " TEXT NOT NULL);";
 }
