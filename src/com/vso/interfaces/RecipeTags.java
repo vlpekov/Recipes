db837608f7ea2eaeb68b1565dce139d34b73be95
vlpekov <vladdey@gmail.com>
2017-06-29 16:02:22 +0300

    new interface: RecipeTags

---
+++ src/com/vso/interfaces/RecipeTags.java
@@ -0,0 +1,11 @@
+package com.vso.interfaces;
+
+public interface RecipeTags {
+	String recipeName = "recipe_name";
+	String cookingDescription = "description";
+	String cookingTime = "cooking_time";
+	String difficulty = "difficulty";
+	String productsList = "products";
+	String portions = "portions";
+	String publishedDate = "published_date";
+}
